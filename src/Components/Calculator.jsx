c2cbe51b5dec6304bbb4634875f87155b9e6fcdf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-08-23 23:43:42 +0530

    added basic ui elememts

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -3,7 +3,35 @@ import React from "react";
 function Calculator() {
   return (
     <>
-      <h2>Helloooooooo</h2>
+      <div className="container">
+        <div>
+          <input></input>
+        </div>
+        <button value="(">(</button>
+        <button value=")">)</button>
+        <button value="%">%</button>
+        <button value="AC">AC</button>
+
+        <button value="7">7</button>
+        <button value="8">8</button>
+        <button value="9">9</button>
+        <button value="*">*</button>
+
+        <button value="4">4</button>
+        <button value="5">5</button>
+        <button value="6">6</button>
+        <button value="-">-</button>
+
+        <button value="1">1</button>
+        <button value="2">2</button>
+        <button value="3">3</button>
+        <button value="+">+</button>
+
+        <button value="+">0</button>
+        <button value="Back">Back</button>
+        <button value="+">=</button>
+        <button value="/">/</button>
+      </div>
     </>
   );
 }
