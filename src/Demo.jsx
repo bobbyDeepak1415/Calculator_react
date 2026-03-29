30a6ef54ce2878ec9e9e57ec6db94c6092d66134
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-30 02:17:18 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,36 +1,11 @@
-import React, { useReducer } from "react";
-import Child from "./Child";
-// import handleClick from "./handleClick";
-
-const initialState = {
-  count: 0,
-};
-
-function reducerFunc(state, action) {
-  switch (action.type) {
-    case "decrement":
-      return { count: state.count - 1 };
-    case "increment":
-      return { count: state.count + 1 };
-    case "reset":
-      return { count: 0 };
-  }
-}
+import React from 'react'
 
 const Demo = () => {
-  console.log("demo rendering...");
-
-  const [state, dispatch] = useReducer(reducerFunc, initialState);
-
   return (
-    <>
-      <h2>Demo count at:{state.count}</h2>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
-      <Child />
-    </>
-  );
-};
+    <div>
+      Hello
+    </div>
+  )
+}
 
-export default Demo;
+export default Demo
--- src/handleClick.jsx
+++
@@ -1,5 +0,0 @@
-const handleClick = (setCount) => {
-  setCount((prev) => prev + 1);
-};
-
-export default handleClick;
