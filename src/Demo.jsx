81eb02b4464cf20a44f2aa47c506d90ccf1168ed
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-30 02:07:03 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,20 @@
-import React, { useCallback, useReducer, useState } from "react";
+import React, {  useReducer } from "react";
 import Child from "./Child";
 // import handleClick from "./handleClick";
+
+const Demo=()=>{
+
+    return (
+
+        <>
+        <h2>Demo count at:{}</h2>
+        <button>-</button>
+        <button>+</button>
+        <button>reset</button>
+        <Child/>
+        </>
+    )
+
+}
 
 export default Demo;
